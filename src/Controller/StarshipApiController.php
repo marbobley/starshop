411b448fc74d1d60092b47ccb6d16acf0e62a09c
wilfried <fournier.wilf@gmail.com>
2025-09-14 19:14:25 +0200

    add log

--- src/Controller/StarshipApiController.php
+++ src/Controller/StarshipApiController.php
@@ -6,18 +6,20 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 use App\Model\Starship;
+use Psr\Log\LoggerInterface;
 
 class StarshipApiController extends AbstractController
 {
     #[Route('/api/starships')]
-    public function getCollection(): Response
+    public function getCollection(LoggerInterface $logger): Response
     {
         $starships = [
             new Starship(1, 'USS LeafyCruiser (NCC-0001)', 'Garden', 'Jean-Luc Pickles', 'taken over by Q'),
             new Starship(2, 'USS Espresso (NCC-1234-C)', 'Latte', 'James T. Quick!', 'repaired'),
             new Starship(3, 'USS Wanderlust (NCC-2024-W)', 'Delta Tourist', 'Kathryn Journeyway', 'under construction'),
         ];
-            
+                    
+        $logger->info('Starship collection retrieved');
 
         return $this->json($starships);
     }
